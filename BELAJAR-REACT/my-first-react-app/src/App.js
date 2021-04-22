dc6c55fc34b1bf600b01da2278cfe940230910c3
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-04-22 17:34:06 +0700

    init dummy object for the data.

--- BELAJAR-REACT/my-first-react-app/src/App.js
+++ BELAJAR-REACT/my-first-react-app/src/App.js
@@ -1,12 +1,45 @@
 import "./App.css"
+import Todos from "./components/Todos"
+import InputSection from "./components/InputSection"
 
 function App() {
-	const user = {
-		name: "rayhan",
-	}
+	const todos = [
+		{
+			id: 1,
+			title: "Belajar Han!!",
+			created: "Wed Apr 14 2021 07:32:10 GMT+0700 (Western Indonesia Time)",
+			status: true,
+		},
+		{
+			id: 2,
+			title: "Buat design ui",
+			created: "Wed Apr 14 2021 07:32:10 GMT+0700 (Western Indonesia Time)",
+			status: false,
+		},
+		{
+			id: 3,
+			title: "Belajar React",
+			created: "Wed Apr 14 2021 07:32:10 GMT+0700 (Western Indonesia Time)",
+			status: true,
+		},
+		{
+			id: 4,
+			title: "Kasih makan kucing",
+			created: "Wed Apr 14 2021 07:32:10 GMT+0700 (Western Indonesia Time)",
+			status: false,
+		},
+		{
+			id: 5,
+			title: "Sumbit course BWA",
+			created: "Wed Apr 14 2021 07:32:10 GMT+0700 (Western Indonesia Time)",
+			status: true,
+		},
+	]
 	return (
-		<div className="App">
-			<h1>{user.name ? `Hello ${user.name}` : "Hello Strangers"}</h1>
+		<div className="App container">
+			<h1>{"Hello Strangers"}</h1>
+			<InputSection />
+			<Todos data={todos} />
 		</div>
 	)
 }
